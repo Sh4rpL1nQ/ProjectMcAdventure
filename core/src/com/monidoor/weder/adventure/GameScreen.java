952ba7f06e45175453ebb4764ac11c57cf97c968
dodoweder97 <you@example.com>
2018-11-18 19:13:29 +0100

    Seperated Screen and Game | Add Hud

---
+++ core/src/com/monidoor/weder/adventure/GameScreen.java
@@ -0,0 +1,58 @@
+package com.monidoor.weder.adventure;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.monidoor.weder.adventure.Scenes.Hud;
+
+public class GameScreen implements Screen {
+
+    private Adventure game;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+
+    public GameScreen(Adventure game) {
+        this.game = game;
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(Adventure.V_WIDTH, Adventure.V_HEIGHT, gameCam);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
