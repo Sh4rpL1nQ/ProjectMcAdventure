7fee075b45862affbdc92decc0e7be5fa4aa3262
dodoweder97 <you@example.com>
2018-11-18 13:12:35 +0100

    Setup LibGdx

---
+++ android/src/com/monidoor/weder/adventure/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.monidoor.weder.adventure;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.monidoor.weder.adventure.Adventure;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Adventure(), config);
+	}
+}
