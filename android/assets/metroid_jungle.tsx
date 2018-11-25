28eae7b9c0975c28365a2a09530e2acf2f04eb54
dodoweder97 <you@example.com>
2018-11-25 14:54:43 +0100

    Include first map attempt with Tiled Deleted Hud.java according to design issues Commented out map input according to file not found issues

---
+++ android/assets/metroid_jungle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="metroid_jungle" tilewidth="16" tileheight="16" tilecount="384" columns="16">
+ <image source="Metroid.png" width="257" height="384"/>
+</tileset>
--- core/src/com/monidoor/weder/adventure/GameScreen.java
+++ core/src/com/monidoor/weder/adventure/GameScreen.java
@@ -4,20 +4,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.monidoor.weder.adventure.Scenes.Hud;
 
 public class GameScreen implements Screen {
 
     private Adventure game;
     private OrthographicCamera gameCam;
     private Viewport gamePort;
+    private TmxMapLoader mapLoader;
+    private TiledMap tileMap;
+    private OrthogonalTiledMapRenderer renderer;
 
     public GameScreen(Adventure game) {
         this.game = game;
         gameCam = new OrthographicCamera();
         gamePort = new FitViewport(Adventure.V_WIDTH, Adventure.V_HEIGHT, gameCam);
+        mapLoader = new TmxMapLoader();
+        //tileMap = mapLoader.load("MetroidWorld1.tmx");
     }
 
     @Override
