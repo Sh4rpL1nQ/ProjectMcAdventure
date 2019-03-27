9bc41f5442185e6e6f62524e30647356e835ce6a
dodoweder97 <you@example.com>
2019-03-27 17:23:04 +0100

    Portal

---
+++ android/assets/rooms/desert.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tmw_desert_spacing" tilewidth="16" tileheight="16" tilecount="192" columns="16">
+ <image source="tmw_desert_spacing.png" width="265" height="199"/>
+</tileset>
